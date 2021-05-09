c1fc2adc4feeb786d9eddffbf6a21d9b0f9cd730
thograce <1304148915@qq.com>
2021-05-09 13:44:36 +0800

    Create MyTrain.py

--- MyTrain.py
+++ MyTrain.py
@@ -57,7 +57,7 @@ def train(train_loader, model, optimizer, epoch):
                   '[lateral-3: {:.4f}]'.
                   format(datetime.now(), epoch, opt.epoch, i, total_step,
                          loss_record3.show()))
-    save_path = 'snapshots/{}/'.format(opt.train_save)
+    save_path = 'checkpoints/{}/'.format(opt.train_save)
     os.makedirs(save_path, exist_ok=True)
     if (epoch+1) % 5 == 0:
         torch.save(model.state_dict(), save_path + 'C2FNet-%d.pth' % epoch)
@@ -104,4 +104,3 @@ if __name__ == '__main__':
     for epoch in range(1, opt.epoch):
         adjust_lr(optimizer, opt.lr, epoch, opt.decay_rate, opt.decay_epoch)
         train(train_loader, model, optimizer, epoch)
-
